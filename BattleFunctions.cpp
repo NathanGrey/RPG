db2966ac6b6982c27c5dfcfc5b27d9159c9654e9
Nathan Grey <Nathan@NathanGrey.com>
2017-05-27 18:07:42 -0700

    Create BattleFunctions.cpp

---
+++ BattleFunctions.cpp
@@ -0,0 +1,25 @@
+#include "BattleFunctions.h"
+#include "Battle.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+/*
+void BattleFunctions::Attack()
+{
+
+	cout << "---BATTLE---------------------------------------------------------------" << endl << endl << endl;
+	cout << "\t" << "You used an attack!" << endl << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------" << endl << endl;
+
+	m_EnemyHealth -= m_PlayerDamage;
+
+	cout << "---BATTLE---------------------------------------------------------------" << endl << endl << endl;
+	cout << "\tYou did " << m_PlayerDamage << " damage to the enemy!" << endl << endl;
+	cout << "\tThe enemy has " << (m_EnemyHealth >= 0 ? m_EnemyHealth : 0) << " HP!" << endl << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------" << endl << endl;
+
+}
+*/
