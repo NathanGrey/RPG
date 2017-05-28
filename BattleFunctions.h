b143dccf71b95df41a17b6aacdc065163bb054d9
Nathan Grey <Nathan@NathanGrey.com>
2017-05-27 18:07:17 -0700

    Create BattleFunctions.h

---
+++ BattleFunctions.h
@@ -0,0 +1,19 @@
+#ifndef BattleFunctions_h
+#define BattleFunctions_h
+#include "Inventory.h"
+#include "Enemy.h"
+#include "Player.h"
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+
+using namespace std;
+
+class BattleFunctions
+{
+private:
+	virtual void Attack();
+
+
+};
+#endif 
