16a588b94c91871daa9ba27ed873a34c92982d89
Nathan Grey <Nathan@NathanGrey.com>
2017-05-27 18:06:31 -0700

    Create Shop.h

---
+++ Shop.h
@@ -0,0 +1,15 @@
+#ifndef Shop_h
+#define Shop_h
+#include <stdio.h>
+class Shop
+{
+public:
+	int m_ShopChoice;
+	int m_Gold;
+	int m_OrangeJuice;
+	int m_StrengthDrink;
+	int Storefront();
+};
+#endif
+
+
